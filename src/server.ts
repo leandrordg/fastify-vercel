47daa16aa35dd1859850b8d74125094ec8e8593e
Leandro Rodrigues <lj251004@gmail.com>
2025-02-20 15:47:02 -0300

    feature: add Swagger documentation support with fastify-swagger and fastify-swagger-ui

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,9 @@
 import { fastifyCors } from "@fastify/cors";
+import { fastifySwagger } from "@fastify/swagger";
+import { fastifySwaggerUi } from "@fastify/swagger-ui";
 import { fastify } from "fastify";
 import {
+  jsonSchemaTransform,
   serializerCompiler,
   validatorCompiler,
   ZodTypeProvider,
@@ -14,11 +17,23 @@ app.setSerializerCompiler(serializerCompiler);
 
 app.register(fastifyCors, { origin: "*" });
 
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: "API com Fastify + TypeScript + Zod",
+      version: "1.0.0",
+    },
+  },
+  transform: jsonSchemaTransform,
+});
+
 app.get("/", async (request, reply) => {
   return reply.status(200).send({ message: "Hello World" });
 });
 
 app.register(usersRoute, { prefix: "/users" });
+
+app.register(fastifySwaggerUi, { routePrefix: "/docs" });
 
 app.listen({ port: 3333 }, () => {
   console.log("Server is running on port 3333");
