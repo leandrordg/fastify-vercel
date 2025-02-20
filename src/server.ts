1f32b38d0cf958175fb79af2009658f10ba8acd9
Leandro Rodrigues <lj251004@gmail.com>
2025-02-20 14:43:40 -0300

    fix: adjust reply handling in root route for consistency

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,12 @@
-import { fastify } from "fastify";
 import { fastifyCors } from "@fastify/cors";
+import { fastify } from "fastify";
 
 const app = fastify();
 
 app.register(fastifyCors, { origin: "*" });
 
 app.get("/", async (request, reply) => {
-    reply.status(200).send({ message: "Hello World" });
+  return reply.status(200).send({ message: "Hello World" });
 });
 
 app.listen({ port: 3333 }, () => {
